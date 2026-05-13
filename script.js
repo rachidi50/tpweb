e4413aac95855ff81ce2167cbf015baa7304f24a
rachidi50 <t.rachidi@esisa.ac.ma>
2026-05-13 16:02:44 +0100

    ff

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("JavaScript fonctionne !");
+const btnMessage = document.getElementById("btnMessage");
+const message = document.getElementById("message");
+
+btnMessage.addEventListener("click", function () {
+  message.textContent = "Bravo, tu as cliqué sur le bouton !";
+});
+const inputNom = document.getElementById("nom");
+const btnNom = document.getElementById("btnNom");
+const resultatNom = document.getElementById("resultatNom");
+
+btnNom.addEventListener("click", function () {
+  resultatNom.textContent = "Bonjour " + inputNom.value;
+});
